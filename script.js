e6ca4da0b57bca13a02d1f7c32a57b6a9c70a5c4
Thais Rosa <tsrosa18@gmail.com>
2022-07-08 03:18:56 -0300

    mudanças na aparencia do codigo

--- script.js
+++
@@ -1,6 +0,0 @@
-// let paletaDeCores = document.getElementById('color-palette');
-// let primeiroBotao = document.createElement('div');
-// primeiroBotao.className = 'color';
-// primeiroBotao.setAttribute('square black small');
-// paletaDeCores.appendchild(primeiroBotao);
-
